f847782ffc8244f02fee35659b751c17f492c146
mspend <mauridobbs@gmail.com>
2026-03-10 14:10:23 -0700

    summarize the number of spots for each bit #

--- notebooks/BioProteanLab_smFISH_one_tile_all_bits_2d.py
+++ notebooks/BioProteanLab_smFISH_one_tile_all_bits_2d.py
@@ -15,54 +15,61 @@ from scipy import ndimage
 path_input = "/data/smFISH/20251028_bartelle_smFISH_mm_microglia_newbuffers/qi2labdatastore/big_fish/tiffs"
 path_output = "/data/smFISH/20251028_bartelle_smFISH_mm_microglia_newbuffers/qi2labdatastore/big_fish/results/one_tile_2D"
 
-# Load in data 
-# These tiffs are the registered, deconvolved image
-path = os.path.join(path_input, "tile000bit005.ome.tiff")
-rna = stack.read_image(path)
-print("smfish channel")
-print("\r shape: {0}".format(rna.shape))
+n_bits = 16
+tile_idx = 0
 
-# polyDT is our fiducial, or reference marker. This probe labels all polyadenylated RNA and is used for CellPose segmentation.
-# We load in this data to visualize the cell boundaries.
-path = os.path.join(path_input, "max_projected_tile000round000corrected_polyDT.ome.tiff")
-polyDT_mip = stack.read_image(path)
-print("polyDT channel")
-print("\r shape: {0}".format(polyDT_mip.shape))
+# All_spots is a list to which we will append all the results of the spot detection
+all_spots = []
 
-# Create a maxiumum intensity projection of the RNA channel
-rna_mip = stack.maximum_projection(rna)
-print("smfish channel (2D maximum projection)")
-print("\r shape: {0}".format(rna_mip.shape))
+# because range is exclusive of the stop
+for bit in range(1, n_bits + 1):
+    # Load in data 
+    # These tiffs are the registered, deconvolved image
+    path = os.path.join(path_input, "tile"+str(tile_idx).zfill(3)+"bit"+str(bit).zfill(3)+".ome.tiff")
+    rna = stack.read_image(path)
 
-# Detect spots in 2D
-# Does not use the polyDT fiducial channel
-spots, threshold = detection.detect_spots(
-    images=rna_mip, 
-    return_threshold=True, 
-    voxel_size=(103, 103),  # in nanometer (one value per dimension yx)
-    spot_radius=(150, 150))  # in nanometer (one value per dimension yx)
+    # Create a maxiumum intensity projection of the RNA channel
+    rna_mip = stack.maximum_projection(rna)
 
-# The function detect_spots returns the coordinates (or list of coordinates) 
-# of the spots with shape (nb_spots, 3) or (nb_spots, 2), for 3-d or 2-d images respectively.
-print("detected spots")
-print("\r shape: {0}".format(spots.shape))
-print("\r threshold: {0}".format(threshold))
+    # Detect spots in 2D
+    # Does not use the polyDT fiducial channel
+    spots, threshold = detection.detect_spots(
+        images=rna_mip, 
+        return_threshold=True, 
+        voxel_size=(103, 103),  # in nanometer (one value per dimension yx)
+        spot_radius=(150, 150))  # in nanometer (one value per dimension yx)
+    
+    spots_df = pd.DataFrame(spots, columns=['y', 'x',])
+    spots_df['bit'] = bit
+    # Append the dataframe of the spots to the list all_spots
+    all_spots.append(spots_df)
+    print(f'Done with bit {bit}')
 
-spots_post_decomposition, dense_regions, reference_spot = detection.decompose_dense(
-    image=rna_mip, 
-    spots=spots, 
-    voxel_size=(103, 103), 
-    spot_radius=(150, 150), 
-    alpha=0.7,  # alpha impacts the number of spots per candidate region
-    beta=1,  # beta impacts the number of candidate regions to decompose
-    gamma=5)  # gamma the filtering step to denoise the image
-print("detected spots before decomposition")
-print("\r shape: {0}".format(spots.shape))
-print("detected spots after decomposition")
-print("\r shape: {0}".format(spots_post_decomposition.shape))
 
-# save in csv files
-# The header of the csv file is y, x, cluster identity #
-spots_df = pd.DataFrame(spots_post_decomposition, columns=['y', 'x',])
+    # spots_post_decomposition, dense_regions, reference_spot = detection.decompose_dense(
+    #     image=rna_mip, 
+    #     spots=spots, 
+    #     voxel_size=(103, 103), 
+    #     spot_radius=(150, 150), 
+    #     alpha=0.7,  # alpha impacts the number of spots per candidate region
+    #     beta=1,  # beta impacts the number of candidate regions to decompose
+    #     gamma=5)  # gamma the filtering step to denoise the image
+    # print("detected spots before decomposition")
+    # print("\r shape: {0}".format(spots.shape))
+    # print("detected spots after decomposition")
+    # print("\r shape: {0}".format(spots_post_decomposition.shape))
+
+    # spots_df = pd.DataFrame(spots_post_decomposition, columns=['y', 'x',])
+
+# Concatenate the spots from all bits
+spots_df = pd.concat(all_spots, ignore_index=True)
+
+# # save in csv files
+# # The header of the csv file is y, x, bit # representing the coordinates of the spot and its identity.
 path = os.path.join(path_output, "spots.csv")
 stack.save_data_to_csv(spots_df, path, delimiter=',')
+
+# Summarize results
+summary = spots_df['bit'].value_counts().reset_index()
+summary = summary.rename(columns={'index' : 'bit', 'bit': 'fish_spots'})
+sorted = summary.sort_values(by='bit')
